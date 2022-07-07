27f48a9c0d44f07f986432d62511117afa981b64
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-07 10:22:50 -0300

    Criado Controller e finalizado projeto

--- src/main/java/br/com/springsemerublog/Greeting.java
+++ src/main/java/br/com/springsemerublog/Greeting.java
@@ -1,5 +1,19 @@
 package br.com.springsemerublog;
 
-public class Greeting {
-
+public class Greeting {		 
+	private final long id;
+    private final String content;
+ 
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+ 
+    public long getId() {
+        return id;
+    }
+ 
+    public String getContent() {
+        return content;
+    }
 }
